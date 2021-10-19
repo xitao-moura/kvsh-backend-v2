f7f206fe7988d88c3c6b132a668ef4036d835223
Xitão Moura <washington.moura@gnis.com.br>
2021-10-19 11:21:52 -0300

    feat

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  port: env.int('PORT', 4001),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '1945e538680ba01c49fd05a269c1f917'),
